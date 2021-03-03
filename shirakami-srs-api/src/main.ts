c57ab4b0208e0898f4260e75720f85465b264980
BeMacized <info@bemacized.net>
2021-03-04 00:38:59 +0100

    Added helmet. Disabled CORS

--- shirakami-srs-api/src/main.ts
+++ shirakami-srs-api/src/main.ts
@@ -2,16 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { buildVersion } from './assets/build-version.json';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   console.log(`Starting ShiraKamiSRS v${buildVersion}`);
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
